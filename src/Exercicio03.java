be890b254dffd475b21876f4784e3ec8303d252a
Jonathan Castro <jonathan.castro18@outlook.com>
2025-06-23 21:25:10 -0300

    Feito o terceiro exercício.

---
+++ src/Exercicio03.java
@@ -0,0 +1,10 @@
+public class Exercicio03 {
+    public static void main(String[] args) {
+        // Escreva um programa Java que imprima os números pares de 2 a 20 usando um
+        // loop for.
+
+        for (int numero = 2; numero <= 20; numero+=2){
+            System.out.println(numero);
+        }
+    }
+}
