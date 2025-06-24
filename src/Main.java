39da690bba2e58c7b0983a4b98fa63939b055c42
Jonathan Castro <jonathan.castro18@outlook.com>
2025-06-23 21:15:46 -0300

    Feito o primeiro exercício.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Exercício 01
+        // Escreva um programa que imprima os números de 10 a 1 usando um loop while.
+
+        int numero = 0;
+
+        int contador = 10;
+
+        while (contador > numero) {
+            System.out.println(contador);
+
+            contador--;
+        }
+    }
+}
