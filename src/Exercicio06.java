d33cb7bf08397d8c64b3c78bbfb06fa8e37fdc77
Jonathan Castro <jonathan.castro18@outlook.com>
2025-07-08 19:47:43 -0300

    Exercício 6 feito.

---
+++ src/Exercicio06.java
@@ -0,0 +1,13 @@
+public class Exercicio06 {
+    public static void main(String[] args) {
+        // Escreva um programa Java que calcule a soma dos dígitos de um número inteiro
+
+        int numero = 96;
+        int numero2 = 6;
+
+
+        int soma = numero + numero2;
+        
+        System.out.println("A soma é " + soma);
+    }
+}
